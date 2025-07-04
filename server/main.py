d3c08e82f9ee634f36dc93a04af9cc3ef111ea77
xtox <xtox@xtoxs-MacBook-Pro.local>
2025-07-04 09:58:18 -0700

    Created individual build functions for different telegram displays

--- server/main.py
+++ server/main.py
@@ -8,15 +8,8 @@ from routes.models import RootResponse
 from lib.Controller import Controller
 from apps.TelegramBot import TelegramBot
 from routes.dependency import set_controller, remove_controller
-from utils.rate_limiter import cleanup_rate_limiter
 from dotenv import load_dotenv
 load_dotenv()
-
-async def periodic_cleanup():
-    """Cleanup rate limiter memory every 10 minutes"""
-    while True:
-        await asyncio.sleep(600)  # 10 minutes
-        cleanup_rate_limiter()
 
 # Initialize the connection to the MongoDB and asign it the global variable
 def initialize_program():
@@ -24,7 +17,6 @@ def initialize_program():
     try:
         # Get and instance of the Controller which is used to read from DB
         controller = Controller()
-        cleanup_task = asyncio.create_task(periodic_cleanup())
         # TODO have the controller poll every 5 minutes for new transactions to insert
 
         # Add controller to dependencies
